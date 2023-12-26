a1fad95edeb735826b7817a422726a46a7038863
Raghib Arshi <46123959+RaghibArshi@users.noreply.github.com>
2023-12-26 10:18:25 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_project/HomeScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
